84c2915467da99da735603cbd11b32cb96be4a16
reughdurgem <reughdurgem@gmail.com>
2017-10-25 04:31:53 -0400

    smaller images for mobile

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -27,3 +27,7 @@ function points() {
         document.getElementById("L").disabled = true;
     }
 }
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    // Take the user to a different screen here.
+    document.write("ayy");
+}
